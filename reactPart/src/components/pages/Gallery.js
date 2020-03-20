1436a79dac6835e6653f7f0e4dd4d07f8d2709cd
Chris <chackx91@yahoo.com>
2020-03-19 20:59:07 -0700

    edits to the gallery

--- reactPart/src/components/pages/Gallery.js
+++ reactPart/src/components/pages/Gallery.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {GEV_Photos, GEV_1, GEV_2, GEV_3, GEV_4, GEV_5, GEV_6, GEV_7, GEV_8} from "../../img";
+import { GEV_Photos, GEV_1, GEV_2, GEV_3, GEV_4, GEV_5, GEV_6, GEV_7, GEV_8 } from "../../img";
 import './gallery.css';
 
 function Gallery() {
@@ -7,35 +7,35 @@ function Gallery() {
     <div>
       <h1>Gallery Page</h1>
       <div class="gallerybox">
-      <div class="row">
-        <div class="col s4">
-          <img class="responsive-img" style={{ height: '400px', width: '320px' }} src={GEV_Photos}></img>
+        <div class="row">
+          <div class="col s4 image">
+            <img class="responsive-img" style={{ height: '320px', width: '320px', borderRadius: '50%' }} src={GEV_Photos}></img>
+          </div>
+          <div class="col s4 image">
+            <img class="responsive-img" style={{ height: '320px', width: '320px', borderRadius: '50%' }} src={GEV_1}></img>
+          </div>
+          <div class="col s4 image">
+            <img class="responsive-img" style={{ height: '320px', width: '320px', borderRadius: '50%' }} src={GEV_2}></img>
+          </div>
+          <div class="col s4 image">
+            <img class="responsive-img" style={{ height: '320px', width: '320px', borderRadius: '50%' }} src={GEV_3}></img>
+          </div>
+          <div class="col s4 image">
+            <img class="responsive-img" style={{ height: '320px', width: '320px', borderRadius: '50%' }} src={GEV_4}></img>
+          </div>
+          <div class="col s4 image">
+            <img class="responsive-img" style={{ height: '320px', width: '320px', borderRadius: '50%' }} src={GEV_5}></img>
+          </div>
+          <div class="col s4 image">
+            <img class="responsive-img" style={{ height: '320px', width: '320px', borderRadius: '50%' }} src={GEV_6}></img>
+          </div>
+          <div class="col s4 image">
+            <img class="responsive-img" style={{ height: '320px', width: '320px', borderRadius: '50%' }} src={GEV_7}></img>
+          </div>
+          <div class="col s4 image">
+            <img class="responsive-img" style={{ height: '320px', width: '320px', borderRadius: '50%' }} src={GEV_8}></img>
+          </div>
         </div>
-        <div class="col s4">
-          <img class="responsive-img" style={{ height: '400px', width: '320px' }} src={GEV_1}></img>
-        </div>
-        <div class="col s4">
-          <img class="responsive-img" style={{ height: '400px', width: '320px' }} src={GEV_2}></img>
-        </div>
-        <div class="col s4">
-          <img class="responsive-img" style={{ height: '400px', width: '320px' }} src={GEV_3}></img>
-        </div>
-        <div class="col s4">
-          <img class="responsive-img" style={{ height: '400px', width: '320px' }} src={GEV_4}></img>
-        </div>
-        <div class="col s4">
-          <img class="responsive-img" style={{ height: '400px', width: '320px' }} src={GEV_5}></img>
-        </div>
-        <div class="col s4">
-          <img class="responsive-img" style={{ height: '400px', width: '320px' }} src={GEV_6}></img>
-        </div>
-        <div class="col s4">
-          <img class="responsive-img" style={{ height: '400px', width: '320px' }} src={GEV_7}></img>
-        </div>
-        <div class="col s4">
-          <img class="responsive-img" style={{ height: '400px', width: '320px' }} src={GEV_8}></img>
-        </div>
-      </div>
       </div>
       <button class="btn waves-effect waves-light" type="submit" name="action">Upload
       </button>
